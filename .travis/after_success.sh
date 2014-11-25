38611acda9e33abcf1c3289b37836a3789387563
Eric Wilson <e.wilson.1@elsevier.com>
2014-11-24 21:52:50 -0500

    pushing to prod git repo on success merge to master

---
+++ .travis/after_success.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+chmod 600 .travis/prod.pem
+ssh-add .travis/prod.pem
+echo "\nHost production\n\tHostName ec2-54-88-185-174.compute-1.amazonaws.com\n\tStrictHostKeyChecking no\n\tUser ubuntu\n\tIdentityFile .travis/prod.pem\n" >> ~/.ssh/config
+cat ~/.ssh/config
+git remote add prod production:/home/ubuntu/titlematch_api.git
+git push prod master
+exit
